d48bcdffefdca5f3675c013b733f1fbe3a5c730b
Matt Lewis <hello@mattlewis.me>
2021-12-14 12:23:55 +0000

    Swap presentational to symbol attributes

--- index.js
+++ index.js
@@ -5,23 +5,7 @@ const fancyLog = require('fancy-log')
 const PluginError = require('plugin-error')
 const Vinyl = require('vinyl')
 
-const presentationAttributes = new Set([
-  'style', 'alignment-baseline', 'baseline-shift', 'clip', 'clip-path',
-  'clip-rule', 'color', 'color-interpolation', 'color-interpolation-filters',
-  'color-profile', 'color-rendering', 'cursor', 'd', 'direction', 'display',
-  'dominant-baseline', 'enable-background', 'fill', 'fill-opacity', 'fill-rule',
-  'filter', 'flood-color', 'flood-opacity', 'font-family', 'font-size',
-  'font-size-adjust', 'font-stretch', 'font-style', 'font-variant',
-  'font-weight', 'glyph-orientation-horizontal', 'glyph-orientation-vertical',
-  'image-rendering', 'kerning', 'letter-spacing', 'lighting-color',
-  'marker-end', 'marker-mid', 'marker-start', 'mask', 'opacity', 'overflow',
-  'pointer-events', 'shape-rendering', 'solid-color', 'solid-opacity',
-  'stop-color', 'stop-opacity', 'stroke', 'stroke-dasharray',
-  'stroke-dashoffset', 'stroke-linecap', 'stroke-linejoin', 'stroke-miterlimit',
-  'stroke-opacity', 'stroke-width', 'text-anchor', 'text-decoration',
-  'text-rendering', 'transform', 'unicode-bidi', 'vector-effect', 'visibility',
-  'word-spacing', 'writing-mode'
-]);
+const symbolAttributes = new Set(['id', 'xml:base', 'xml:lang', 'xml:space', 'onfocusin', 'onfocusout', 'onactivate', 'onclick', 'onmousedown', 'onmouseup', 'onmouseover', 'onmousemove', 'onmouseout', 'onload', 'alignment-baseline', 'baseline-shift', 'clip', 'clip-path', 'clip-rule', 'color', 'color-interpolation', 'color-interpolation-filters', 'color-profile', 'color-rendering', 'cursor', 'direction', 'display', 'dominant-baseline', 'enable-background', 'fill', 'fill-opacity', 'fill-rule', 'filter', 'flood-color', 'flood-opacity', 'font-family', 'font-size', 'font-size-adjust', 'font-stretch', 'font-style', 'font-variant', 'font-weight', 'glyph-orientation-horizontal', 'glyph-orientation-vertical', 'image-rendering', 'kerning', 'letter-spacing', 'lighting-color', 'marker-end', 'marker-mid', 'marker-start', 'mask', 'opacity', 'overflow', 'pointer-events', 'shape-rendering', 'stop-color', 'stop-opacity', 'stroke', 'stroke-dasharray', 'stroke-dashoffset', 'stroke-linecap', 'stroke-linejoin', 'stroke-miterlimit', 'stroke-opacity', 'stroke-width', 'text-anchor', 'text-decoration', 'text-rendering', 'unicode-bidi', 'visibility', 'word-spacing', 'writing-mode', 'class', 'style', 'externalResourcesRequired', 'preserveAspectRatio', 'viewBox', 'aria-labelledby']);
 
 module.exports = function (config) {
 
@@ -130,7 +114,7 @@ module.exports = function (config) {
     }
 
     for (let [name, value] of Object.entries($svg.attr())) {
-      if (!presentationAttributes.has(name)) continue;
+      if (!symbolAttributes.has(name)) continue;
       $symbol.attr(name, value)
     }
 
